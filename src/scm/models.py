acef234247aeb84247fcf2e30cc23f58972ff2a4
Nick Ban <nick.ban@qq.com>
2020-01-16 19:30:45 +1100

    create scm/user model and migrate

--- src/scm/models.py
+++ src/scm/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    is_factory = models.BooleanField(default=False)
+    is_merchandiser = models.BooleanField(default=False)
+    is_designer = models.BooleanField(default=False)
+    is_finance = models.BooleanField(default=False)
+    is_shipping = models.BooleanField(default=False)
+    is_office = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_merchandiser_manager = models.BooleanField(default=False)
