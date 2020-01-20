7e81cff3ef49cfbf5ab3dcea340c4818badb720b
Nick Ban <nick.ban@qq.com>
2020-01-20 14:47:02 +1100

    change login page

--- src/scm/forms.py
+++ src/scm/forms.py
@@ -4,10 +4,6 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext, ugettext_lazy as _
 
 class MyAuthenticationForm(AuthenticationForm):
-    username = forms.CharField(label=_("用户名"), 
-                               max_length=254)
-    password = forms.CharField(label=_("密码"), 
-                               widget=forms.PasswordInput)
     error_messages = {
         'invalid_login': _("用户名和密码不匹配，请重新输入!"),
         'inactive': _("该账号已被冻结!"),
--- src/scm/views.py
+++ src/scm/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView
 from .forms import MyAuthenticationForm
 
+
 class HomeView(TemplateView):
     template_name = 'home.html'
 
