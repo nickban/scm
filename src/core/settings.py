de649055c3add03d71379a48744d499f8dc1be8c
Nick Ban <nick.ban@qq.com>
2020-01-20 12:24:33 +1100

    make simple login

--- src/core/settings.py
+++ src/core/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'scm',
+    'crispy_forms',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- src/core/urls.py
+++ src/core/urls.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from scm.forms import MyAuthenticationForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(authentication_form=MyAuthenticationForm), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', include('scm.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
