8f907a4ea53960e0def9af26850d41e543c414be
Nick Ban <nick.ban@qq.com>
2020-01-29 09:43:17 +1100

    post template move.

--- src/core/settings.py
+++ src/core/settings.py
@@ -57,7 +57,8 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'templates/post/'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
