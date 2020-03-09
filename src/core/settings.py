309b01a7758a034ff238b9500ee96c0ef6813eee
Nick Ban <nick.ban@qq.com>
2020-03-09 15:49:08 +1100

    update setting

--- src/core/settings.py
+++ src/core/settings.py
@@ -114,10 +114,10 @@ USE_TZ = True
 
 # 静态文件设置
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles/')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # 用户登录授权模块
 AUTH_USER_MODEL = 'scm.User'
