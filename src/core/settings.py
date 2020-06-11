9c67ffcfb4b0c62051c6aa7cf31154c225eb6e8c
nick <nick@nicks-MacBook-Pro.local>
2020-06-11 16:43:34 +1000

    udpate setting

--- src/core/settings.py
+++ src/core/settings.py
@@ -18,7 +18,7 @@ else:
 
 # Application definition
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
