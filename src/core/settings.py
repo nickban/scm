c103ccf93b86af35f5291f9f3eb36326a39f81a7
nick <nick@nicks-MacBook-Pro.local>
2020-06-16 00:20:39 +1000

    setting

--- src/core/settings.py
+++ src/core/settings.py
@@ -2,6 +2,8 @@ import os
 from decouple import config
 from django.contrib.messages import constants as messages
 
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
