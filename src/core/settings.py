f607b215e7caa8a6a5f544a99ebde9e28bf3a239
nick <nick@nicks-MacBook-Pro.local>
2020-07-16 21:21:31 +1000

    setting

--- src/core/settings.py
+++ src/core/settings.py
@@ -16,7 +16,7 @@ DEBUG = False
 if DEBUG:
     ALLOWED_HOSTS = ['127.0.0.1']
 else:
-    ALLOWED_HOSTS = ['198.211.112.89']
+    ALLOWED_HOSTS = ['198.211.112.89','scm.monayoung.com.au']
 
 # Application definition
 INSTALLED_APPS = [
